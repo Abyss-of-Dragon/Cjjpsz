03a1e71b704b215e38283aecda94355caf008b73
Temperanci <temperance131@gmail.com>
2021-06-01 14:13:52 +0800

    help
    I need someone to finish login function

--- ProjectCode/Projdemo/src/main/java/com/ProjdemoApplication.java
+++ ProjectCode/Projdemo/src/main/java/com/ProjdemoApplication.java
@@ -1,14 +1,14 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-@SpringBootApplication
+
 @ComponentScan("com.service")
+@SpringBootApplication
+
 public class ProjdemoApplication {
 
     public static void main(String[] args) {
--- ProjectCode/Projdemo/src/main/java/com/controller/LoginController.java
+++ ProjectCode/Projdemo/src/main/java/com/controller/LoginController.java
@@ -2,10 +2,7 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import com.entity.User;
 import com.service.UserService;
 
